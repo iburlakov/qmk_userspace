0131b1e23dcdd5426f9d3144aefcadff83fbdd76
Ivan Burlakov <iburlakov@gmail.com>
2025-11-10 16:32:44 +0100

    Add German keymap support for Windows and Mac layouts

---
+++ keyboards/keycapsss/3w6_2040/keymaps/de/keymap_german_sym.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+These are aliases for symbols for windows german layout.
+*******************************************************************************/
+
+#pragma once
+#include "keycodes.h"
+// clang-format off
+
+// Aliases
+#define DE_LCBR_WIN ALGR(DE_7)    // {
+#define DE_LBRC_WIN ALGR(DE_8)    // [
+#define DE_RBRC_WIN ALGR(DE_9)    // ]
+#define DE_RCBR_WIN ALGR(DE_0)    // }
+#define DE_BSLS_WIN ALGR(DE_SS)   // (backslash)
+#define DE_AT_WIN ALGR(DE_Q)    // @
+#define DE_TILD_WIN ALGR(DE_PLUS) // ~
+#define DE_PIPE_WIN ALGR(DE_LABK) // |
